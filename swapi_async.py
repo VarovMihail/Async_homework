aef749fd12f311ce4b53e1e3ddfe98471293c5a7
Варов Михаил <8605495@mail.ru>
2022-12-18 21:19:56 +0300

    add def my_list

--- swapi_async.py
+++ swapi_async.py
@@ -1,32 +1,44 @@
-from typing import Iterator, AsyncIterator, Coroutine
-
+from typing import AsyncIterator, Coroutine
+import requests
 from aiohttp import ClientSession
-import json
 import asyncio
-import time
 from more_itertools import chunked
 from db import engine, Session, People, Base
+import time
+
 
 CHUNK_SIZE = 10
 
-async def get_person(id):
+async def my_list(link_dict: dict):
+    """Асинхронные запросы по ссылкам films, vehicles, starships, species"""
+    list_of_dicts = []
+    for link in link_dict:
+        async with ClientSession() as session:
+            response = await session.get(link)
+        item_dict = await response.json()
+        print(type(item_dict))
+        list_of_dicts.append(item_dict)
+    return list_of_dicts
+
+
+async def get_person(person_id: int) -> dict:
     """Получить одного персонажа и исправить его словарь"""
     async with ClientSession() as session:
-        response = await session.get(f'https://swapi.dev/api/people/{id}')
+        response = await session.get(f'https://swapi.dev/api/people/{person_id}')
 
     person_dict = await response.json()
 
     if person_dict.get('name'):
-        person_dict['vehicles'] = ','.join(person_dict['vehicles'])
-        person_dict['starships'] = ','.join(person_dict['starships'])
-        person_dict['species'] = ','.join(person_dict['species'])
-        person_dict['films'] = ','.join(person_dict['films'])
+        person_dict['films'] = ','.join([i['title'] for i in await my_list(person_dict['films'])])
+        person_dict['vehicles'] = ','.join([i['name'] for i in await my_list(person_dict['vehicles'])])
+        person_dict['starships'] = ','.join([i['name'] for i in await my_list(person_dict['starships'])])
+        person_dict['species'] = ','.join([i['name'] for i in await my_list(person_dict['species'])])
         del person_dict['created']
         del person_dict['edited']
         del person_dict['url']
         return person_dict
     else:
-        print('пустой персонаж под номером', id)
+        print('пустой персонаж под номером', person_id)
         return {}
 
 
@@ -34,18 +46,19 @@ async def chunk_people(start: int, end: int) -> AsyncIterator[dict]:
     """Разбить количество героев на куски и сделать генератор"""
     for chunk in chunked(range(start, end), CHUNK_SIZE):
         coroutines: list[Coroutine] = [get_person(i) for i in chunk]
-        people = await asyncio.gather(*coroutines)
+        people: tuple = await asyncio.gather(*coroutines)
         for person_dict in people:
             yield person_dict
 
-async def add_people_to_db(person_dict_buffer):
+
+async def add_people_to_db(person_dict_buffer: list):
     """Вставить кусок в базу данных"""
     async with Session() as session:
-
         people_instance = [People(**person_dict) for person_dict in person_dict_buffer]
         print(len(people_instance), 'героев грузим в базу')
         session.add_all(people_instance)
         await session.commit()
+
 
 async def main():
     """Основная функция"""
@@ -61,7 +74,11 @@ async def main():
 
         if len(person_dict_buffer) >= 10:
             asyncio.create_task(add_people_to_db(person_dict_buffer))
-            person_dict_buffer.clear()
+            # person_dict_buffer.clear()
+            # В работе с памятью перемудрили. отчищается тот же самый список,
+            # который идет на вставку, которая идет паралельно с основным кодом.
+            # Просто создавайте новый пустой список
+            person_dict_buffer = []
 
     if person_dict_buffer:
         await add_people_to_db(person_dict_buffer)
@@ -73,4 +90,8 @@ async def main():
 
     await engine.dispose()
 
-asyncio.run(main())
+
+if __name__ == '__main__':
+    start = time.monotonic()
+    asyncio.run(main())
+    print(time.monotonic() - start)
